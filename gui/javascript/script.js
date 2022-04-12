ccf026becf08ef39c86595f78d7f7d36f490829b
carlottahillger <78755376+carlottahillger@users.noreply.github.com>
2022-04-12 13:18:10 +0200

    #9 update add example block
    Co-Authored-By: gusniage02 <90870707+gusniage02@users.noreply.github.com>

--- gui/javascript/script.js
+++ gui/javascript/script.js
@@ -7,6 +7,20 @@ function main() {
     CONTEXT = CANVAS.getContext("2d")
 }
 
+//The example blocks are the blocks in the menu that we cannot move
+class EXAMPLE_BLOCK{
+    constructor(typeOfMovement, angle, xPosition, yPosition, width, height){
+        this.xPosition=xPosition;
+        this.yPosition=yPosition;
+        this.typeOfMovement=typeOfMovement;
+        this.angle=angle;
+        this.width=width;
+        this.height=height
+    }
+}
+
+//The blocks are the ones which are created when a mouse click is detected on the example blocks and
+//which can be dragged into the canvas.
 class BLOCK{
     constructor(typeOfMovement, angle, xPosition, yPosition, width, height){
         this.xPosition=xPosition;
