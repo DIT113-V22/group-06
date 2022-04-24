09b075e5f3f000437679e01db2fd8be53504a659
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2022-04-24 13:43:37 +0200

    #16 Fix drop styling
    Added top padding to the code blocks when dropped in the container.
    
    Added colors to the code blocks when dropped in the container.

--- gui/javascript/script.js
+++ gui/javascript/script.js
@@ -15,16 +15,17 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     var isLeft = 'move-forward' == data || "move-backwards" == data || "move-left" == data || "move-right" == data;
     var nodeCopy = document.getElementById(data).cloneNode(true);
-    nodeCopy.id = "form" + ev.target.id;
+    //nodeCopy.id = "form" + ev.target.id;
     // clean target space if needed
+    
     if (isLeft) {
-        /*
+        
         if (ev.target.nodeName == 'FORM') {
         ev.target.parentNode.appendChild(nodeCopy);
         removeNode(ev.target);
         }
         else 
-        */
+        
         ev.target.appendChild(nodeCopy);
     }
     else {
@@ -35,4 +36,6 @@ function drop(ev) {
     }
     ev.stopPropagation();
     return false;
+
+    
  }
