4088c269edd615959884c245bb27a097316ff727
carlottahillger <78755376+carlottahillger@users.noreply.github.com>
2022-04-25 15:03:02 +0200

    #16 fixed bug, simplified code and added comments
    I fixed the bug of being able to drag blocks into other blocks and that they disappear when dragged into the "steps" field. I also tried to simplify the code a little, deleted redundant code and made some comments.

--- gui/javascript/script.js
+++ gui/javascript/script.js
@@ -1,40 +1,43 @@
+const canvas = document.getElementById("canvas-container")
+
+
+//with this function we allow elements to be dropped in a certain place.
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
+//this function allows us to drag elements from one place to another
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
+//with this method we remove a node (one block)
 function removeNode(node) {
     node.parentNode.removeChild(node);
 }
-      
+  
+//This function is to drop an element in the canvas
 function drop(ev) {
-    
-    
-    
     ev.preventDefault();
+    //we get the data we set in the drag() method
     var data = ev.dataTransfer.getData("text");
+    //we check whether the selected block is inside the selection menu or in the canvas and make a copy of it
     var isLeft = 'move-forward' == data || "move-backwards" == data || "move-left" == data || "move-right" == data;
     var nodeCopy = document.getElementById(data).cloneNode(true);
-    nodeCopy.style.cssText += 'width: 100%';
 
+    //we check whether we try to drop it on the canvas (otherwise we can also drop inside the other blocks)
+    if(ev.target.id == "canvas"){
+        if (isLeft) {
+            //we need different id´s for the elements in the menu and the ones in the canvas
+            nodeCopy.id = data + "-copy";
+            ev.target.appendChild(nodeCopy);
 
-    // clean target space if needed
-    if (isLeft) {
-        nodeCopy.id = data + "-copy";
-        if (ev.target.nodeName == 'FORM') {
-        ev.target.parentNode.appendChild(nodeCopy);
-        removeNode(ev.target);
         }
-        else 
-        ev.target.appendChild(nodeCopy);
-    }
-    else {
-        if (ev.target.nodeName != 'FORM') {
-        removeNode(document.getElementById(data));
-        ev.target.appendChild(nodeCopy);
+        else {
+            //right now we can only add to the end of the document. We remove the element and add it again at the end
+            removeNode(document.getElementById(data));
+            ev.target.appendChild(nodeCopy);
+            
         }
     }
     ev.stopPropagation();
