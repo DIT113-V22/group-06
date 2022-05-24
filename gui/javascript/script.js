9c5afa7e65053be1183253e4797379067e71b696
carlottahillger <78755376+carlottahillger@users.noreply.github.com>
2022-05-24 13:49:54 +0200

    #51 fixed bugs for drag into "repeat x times"

--- gui/javascript/script.js
+++ gui/javascript/script.js
@@ -96,6 +96,16 @@ window.drop = function drop (ev) {
     // we set the class to dragging so that we can distinguish it from the others
     // we check whether we try to drop it on the canvas (otherwise we can also drop inside the other blocks)
     const elementAfter = getElementAfter(ev.clientY, true, null)
+
+    if (data === 'repeat') {
+      console.log("here")
+      //nodeCopy.children[3].ondrop = "dropInRepeat(event)"
+      //nodeCopy.children[3].ondragover = "allowDrop(event)"
+      nodeCopy.children[3].setAttribute('ondrop', 'dropInRepeat(event)')
+      nodeCopy.children[3].setAttribute('ondragover', 'allowDrop(event)')
+      console.log(nodeCopy.children[3])
+    }
+
     if (isLeft && ev.target.id !== 'trash-icon') {
       // we need different id´s for the elements in the menu and the ones in the canvas
       nodeCopy.id = data + '-copy'
@@ -115,23 +125,25 @@ window.drop = function drop (ev) {
 
 window.dropInRepeat = function dropInRepeat (ev) {
   ev.preventDefault()
-  const repeatBlock = document.getElementById('canvas').querySelectorAll(".repeat-canvas").item(0)
+  // const selectedBlock =  getRepeatBlock(ev.clientY)
+  const repeatBlock = getRepeatBlock(ev.clientY).children[3]
+
   console.log(repeatBlock)
   // we get the data we set in the drag() method
   const data = ev.dataTransfer.getData('text')
   // we check whether the selected block is inside the selection menu or in the canvas and make a copy of it
   const isLeft = (data === 'move-forward' || data === 'move-backwards' || data === 'move-left' || data === 'move-right' || data === 'turn-around' || data === 'spin' || data === 'wait' || data === 'repeat')
-  const isRepeatBlock = (data === 'repeat')
   const nodeCopy = document.getElementsByClassName('dragging').item(0).cloneNode(true)
+  
   // we set the class to dragging so that we can distinguish it from the others
   // we check whether we try to drop it on the canvas (otherwise we can also drop inside the other blocks)
   const elementAfter = getElementAfter(ev.clientY, false, repeatBlock)
   
-  if (isLeft && ev.target.id !== 'repeat') {
+  if (isLeft && data !== 'repeat' && data !== 'repeat-copy') {
     // we need different id´s for the elements in the menu and the ones in the canvas
     nodeCopy.id = data + '-copy'
     checkAndInsertElement(elementAfter, nodeCopy, repeatBlock)
-  } else if (ev.target.id !== 'repeat') {
+  } else if (data !== 'repeat' && data !== 'repeat-copy') {
     // We check which element would come after the position we are dropping the element, remove the element and append it on the right position
     removeNode(document.getElementsByClassName('dragging').item(0))
     checkAndInsertElement(elementAfter, nodeCopy, repeatBlock)
@@ -167,6 +179,7 @@ function getElementAfter (y, canvas, repeatBlock) {
   } else {
     remainingBlocks = [...repeatBlock.querySelectorAll('.block:not(.dragging)')]
   }
+  console.log(remainingBlocks)
   // check which element is closest after(-> offset below 0) the current position and return it
   return remainingBlocks.reduce((closest, child) => {
     const box = child.getBoundingClientRect()
@@ -177,6 +190,28 @@ function getElementAfter (y, canvas, repeatBlock) {
       return closest
     }
   }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+function getRepeatBlock (y) {
+  const canvas = document.getElementById('canvas')
+  const blocks = canvas.querySelectorAll('.block')
+  let offset = 1000000
+  let blockToReturn = null
+  let newOffset = 1000000
+  for (let i = 0; i < blocks.length; i++) {
+    if (blocks.item(i).id === 'repeat-copy') {
+      box = blocks.item(i).getBoundingClientRect()
+      newOffset = y - box.top - box.height / 2
+      if (newOffset < 0) {
+        newOffset = newOffset * -1
+      }
+      if (offset > newOffset) {
+        offset = newOffset
+        blockToReturn = blocks.item(i)
+      }
+    }
+  }
+  return blockToReturn
 }
 
 /// Function to get the text of all code blocks in the canvas
